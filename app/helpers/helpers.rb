09427c9295b40b5d203c7be9b61ac599ddc0b81c
Stephen <stephenjsweets@gmail.com>
2021-01-29 20:25:23 -0500

    commit tree 7218ffb6940e9ed44faf1da18646f07fc786d831

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+
+    def self.current_user(args)
+        @user = User.find(args[:user_id])
+    end
+
+    def self.is_logged_in?(args)
+        !!args[:user_id]
+    end
+    
 end
